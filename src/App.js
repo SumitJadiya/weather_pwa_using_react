aded1547609980315300a75682746ae92dfb778c
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-07-17 18:11:58 +0530

    integration with weather API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { fetchWeather } from "./api/fetchWeather"
+import "./App.css"
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [query, setQuery] = useState('')
+    const [weather, setWeather] = useState({})
+
+    const search = async (e) => {
+        if (e.key === "Enter") {
+            const data = await fetchWeather(query)
+            setWeather(data)
+            setQuery('')
+
+            console.log(data)
+        }
+    }
+
+    return (
+        <div className="main-container">
+            <input
+                type="text"
+                className="search"
+                placeholder="Search Your Place.."
+                value={query}
+                onChange={e => setQuery(e.target.value)}
+                onKeyPress={search}
+            />
+
+            {weather.main && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span> <sup>{weather.sys.country}</sup>
+                    </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                    <div className="city-wind">
+
+                        Wind Speed : {Math.round(weather.wind.speed)}<span> kmph</span>
+
+                    </div>
+                    <div className="info">
+                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <p>{weather.weather[0].description}</p>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
 }
 
-export default App;
+export default App
